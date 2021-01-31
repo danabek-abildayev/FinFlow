02ebe9829fce845f3edd1796feb4e4bdb740a8b1
Danabek Abildayev <danabek.abildayev@gmail.com>
2021-01-31 19:56:10 +0500

    Fix text field hiding under keyboard issue.

--- FinFlow/Controllers/NewTransactionViewController.swift
+++ FinFlow/Controllers/NewTransactionViewController.swift
@@ -30,9 +30,20 @@ class NewTransactionViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         view.backgroundColor = .gray
-        
         setItems()
         
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(sender:)), name: UIResponder.keyboardWillShowNotification, object: nil);
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name: UIResponder.keyboardWillHideNotification, object: nil);
+        
+    }
+    
+    @objc func keyboardWillShow(sender: NSNotification) {
+         self.view.frame.origin.y = -150 // Move view 150 points upward
+    }
+
+    @objc func keyboardWillHide(sender: NSNotification) {
+         self.view.frame.origin.y = 0 // Move view to original position
     }
     
     func setItems() {
